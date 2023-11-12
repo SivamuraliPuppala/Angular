4c0af6879c491bc0b226610405dcc3a7704b3fcf
SivamuraliPuppala <sivamurali.kp89@gmail.com>
2023-11-12 00:03:26 -0600

    firstpublish

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Title changed for my first app';
+  name='Murali Krishna';
+  cricInfo='live cricket scores';
+}
